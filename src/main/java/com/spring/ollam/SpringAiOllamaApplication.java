e3a6e2898782e821c895d8f3c4468a8b12dd58ac
Deepak Hudge <100028762+Hudgedeepak@users.noreply.github.com>
2026-04-02 22:43:06 +0530

    Initial commit

---
+++ src/main/java/com/spring/ollam/SpringAiOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ollam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOllamaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/ollam/controllers/ChatController.java
@@ -0,0 +1,26 @@
+package com.spring.ollam.controllers;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/")
+public class ChatController {
+
+   private ChatClient chatClient; //it gives the response, it has all method. we did not get direct bean , we have create builder bean than we use it.
+
+   public ChatController(ChatClient.Builder builder) {
+       this.chatClient=builder.build();
+   }
+
+   @GetMapping("/chat")
+   public ResponseEntity<String> chatMessage(@RequestParam(value = "q", required = true) String message){
+
+      String resultResponse = chatClient.prompt(message).call().content();
+      return ResponseEntity.ok(resultResponse);
+   }
+}
